7f6ed62f9ef8c9067498d46e2f342c1750aeb22c
Alexandre Lessard <tmdudydude@hotmail.com>
2018-12-21 17:18:47 -0500

    day6

--- Day 06/day06.py
+++ Day 06/day06.py
@@ -1,17 +1,6 @@
-def calculateManhattanDistance(x1, x2, y1, y2):
-    x = [x1, x2]
-    y = [y1, y2]
-    x1 = max(x)
-    x2 = min(x)
-    y1 = max(y)
-    y2 = min(y)
-    #print(x1)
-    #print(x2)
-    #print(y1)
-    #print(y2)
-    return (x1 - x2) + (y1-y2)
+import os
 
-filename = "day06 example.txt"
+filename = "day06 input.txt"
 input = open(filename)
 
 lines = input.read().split("\n")
@@ -21,6 +10,8 @@ xs = []
 ys = []
 
 grid = {}
+
+owners = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 for line in lines:
     coordinates = line.split(",")
@@ -35,16 +26,14 @@ maxY = max(ys)
 minX = min(xs)
 minY = min(ys)
 
-maxX += 1
-maxY += 1
+maxX += 2
+maxY += 2
 
 minX -= 1
 minY -= 1
 
 for i, val in enumerate(xs):
-    grid[str(xs[i]) + "," + str(ys[i])] = 0
-
-print(grid)
+    grid[str(xs[i]) + "," + str(ys[i])] = {"distance" : 0, "owner" : i}
 
 end = False
 iteration = 1
@@ -55,9 +44,7 @@ y = 0
 while end == False:
     change = False
     for i, val in enumerate(xs): #pour chaque point central
-        print("xs[i] : " + str(xs[i]))
-        print("ys[i] : " + str(ys[i]))
-        for stage in range(0, 3):
+        for stage in range(0, 4):
             if stage == 0:
                 x = iteration
                 y = (-1 * iteration)
@@ -78,16 +65,61 @@ while end == False:
                 y = (-1 * iteration)
                 xDirection = 1
                 yDirection = 0
-            for step in range(0, iteration * 2):
-                #print("x : " + str(xs[i] + xDirection * step + x))
-                #print("y : " + str(ys[i] + yDirection * step + y))
+            #print("x : " + str(x))
+            #print("y : " + str(y))
+            #print("xDirection : " + str(xDirection))
+            #print("yDirection : " + str(yDirection))
+            #os.system("pause")
+            for step in range(0, iteration * 2 +1):
+                name = str(xs[i] + xDirection * step + x) + "," + str(ys[i] + yDirection * step + y)
+                #print(name)
+                #os.system("pause")
                 if (xs[i] + xDirection * step + x) <= maxX and (xs[i] + xDirection * step + x) >= minX and (ys[i] + yDirection * step + y) <= maxY and (ys[i] + yDirection * step + y) >= minY:
-                    if str(xs[i] + xDirection * step + x) + "," + str(ys[i] + yDirection * step + y) in grid:
-                        grid[str(xs[i] + xDirection + x) + "," + str(ys[i] + yDirection * step + y)] = calculateManhattanDistance((xs[i] + xDirection * step + x), xs[i], (ys[i] + yDirection * step + y), ys[i])
+                    Ax = xs[i]
+                    Bx = xs[i] + xDirection * step + x
+                    Ay = ys[i]
+                    By = ys[i] + yDirection * step + y
+                    distance = abs(Ax - Bx) + abs(Ay - By)
+                    if name in grid:
+                        if grid[name]["distance"] == distance:
+                            if grid[name]["owner"] != i:
+                                grid[name]["owner"] = "."
+                        elif grid[name]["distance"] > distance:
+                            grid[name]["distance"] = distance
+                            grid[name]["owner"] = i
                     else:
-                        grid[str(xs[i] + xDirection * step + x) + "," + str(ys[i] + yDirection * step + y)] = calculateManhattanDistance((xs[i] + xDirection * step + x), xs[i], (ys[i] + yDirection * step + y), ys[i])
+                        grid[name] = {"distance" : distance, "owner" : i}
                     change = True
-                print(grid)
+    #for yi in range(minY, maxY):
+     #   string = ""
+      #  for xi in range(minX, maxX):
+       #     name = str(xi) + "," + str(yi)
+        #    if name in grid:
+         #       string += str(grid[name]["owner"])
+          #  else:
+           #     string += " "
+        #print(string)
+    #os.system("pause")
     if change == False:
         end = True
     iteration += 1
+
+#for y in range(minY, maxY):
+ #   string = ""
+  #  for x in range(minX, maxX):
+   #     name = str(x) + "," + str(y)
+    #    if grid[name]["distance"] != 0:
+     #       if grid[name]["owner"] == ".":
+      #          string += grid[name]["owner"]
+       #     else:
+        #        string += owners[int(grid[name]["owner"])].lower()
+        #else:
+        #    string += owners[int(grid[name]["owner"])].upper()
+    #print(string)
+
+results = {}
+
+for location in grid:
+
+for result in results:
+    print(result)
